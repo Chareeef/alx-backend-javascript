2a19a19704b1b60f1fded421ff16cd549eceb0ef
Chareeef <youssef.charif.h@gmail.com>
2024-03-04 18:13:29 +0100

    Add 12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
+    },
+  };
+}
