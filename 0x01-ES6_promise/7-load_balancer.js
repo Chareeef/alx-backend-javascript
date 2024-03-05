4b2b2771a277c2fe503ad0d867a7b3c2b444e4ca
Chareeef <youssef.charif.h@gmail.com>
2024-03-05 22:20:57 +0100

    Add 7-load_balancer.js

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,6 @@
+// Return the value returned by the promise that resolved the first.
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise
+    .race([chinaDownload, USDownload])
+    .then((value) => value);
+}
