5cdd1f995c8cf66275b9c845cf8e9b161f37b555
Chareeef <youssef.charif.h@gmail.com>
2024-03-07 15:34:23 +0100

    Implement Pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-underscore-dangle */
+
+// `Pricing` class implementation
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // amount getter
+  get amount() {
+    return this._amount;
+  }
+
+  // amount setter
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // currency getter
+  get currency() {
+    return this._currency;
+  }
+
+  // currency setter
+  set currency(cur) {
+    this._currency = cur;
+  }
+
+  // Display full price (ex: `20 Dollars ($)`)
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  // Convert the price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
