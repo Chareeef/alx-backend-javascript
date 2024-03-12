dd1f6951eecc2b522fd1bf0f48e239498dbf872f
Chareeef <youssef.charif.h@gmail.com>
2024-03-12 00:37:27 +0000

    Generate groceries map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+// Returns a map of groceries with the following items (name, quantity)
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
+console.log(groceriesList());
