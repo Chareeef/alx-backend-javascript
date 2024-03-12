91786875af7df32f814581f6adb35bf0655d1abd
Chareeef <youssef.charif.h@gmail.com>
2024-03-12 01:30:26 +0000

    Track API queries with WeakMap

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -8,6 +8,3 @@ export default function hasValuesFromArray(set, array) {
 
   return true;
 }
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
