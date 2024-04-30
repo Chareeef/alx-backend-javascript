4752f9925b46c17b45200d05848bb5b94e451265
Chareeef <youssef.charif.h@gmail.com>
2024-04-30 15:55:13 +0100

    Implement readDatabase func in full_server/utils.js

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,34 @@
+import { readFile } from 'fs';
+
+// Read database asycnchronously
+export default function readDatabase(path) {
+  // Return a promise
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf-8', ((err, data) => {
+      // Reject if an error occured
+      if (err) {
+        reject(err);
+      } else { // Recieved data
+        const lines = data.split('\n').filter((line) => line !== '').slice(1);
+        const students = lines.map((line) => line.split(','));
+
+        // Group students by fields
+        const studentsByFields = {};
+
+        students.forEach((item) => {
+          const firstName = item[0];
+          const field = item[3];
+
+          if (!studentsByFields[field]) {
+            studentsByFields[field] = [firstName];
+          } else {
+            studentsByFields[field].push(firstName);
+          }
+        });
+
+        // Resolve studentsByFields
+        resolve(studentsByFields);
+      }
+    }));
+  });
+}
