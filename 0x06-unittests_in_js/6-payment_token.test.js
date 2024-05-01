d3e51e419193e4693780d386c9d9f8704d61f3d6
Chareeef <youssef.charif.h@gmail.com>
2024-05-01 21:11:02 +0100

    Test getPaymentTokenFromAPI using done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,11 @@ const chai = require('chai');
 const expect = chai.expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-chai.use(require('chai-as-promised'));
-
 describe('Test getPaymentTokenFromAPI function', () => {
-  it('Test if we pass `true`', () => {
-    return expect(getPaymentTokenFromAPI(true)).to.eventually.deep.equal({ data: 'Successful response from the API' });
+  it('Test if we pass `true`', (done) => {
+    getPaymentTokenFromAPI(true).then((resp) => {
+      expect(resp).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    });
   });
 });
